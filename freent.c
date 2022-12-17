8247f8b26cb28f148799eb9020dcfad6351d1097
Tyler Magee Shields <115214762+TylerMS887@users.noreply.github.com>
2022-12-17 11:55:55 +0000

    Update freent.c

--- freent.c
+++ freent.c
@@ -1,4 +1,4 @@
 // FreeNT Kernel
 // 
 // This software comes with ABSOLUTELY NO WARRANTY. See the OpenWindows License
-// for details. https://github.com/freent-project/freent/raw/main/LICENSE
+// for details. https://github.com/freent-project/license-translations
