c18b75d6e8b1bdb0a85a0d849b10bbb03caae12b
Tyler Magee Shields <115214762+TylerMS887@users.noreply.github.com>
2022-12-17 15:59:53 +0000

    Update freent.c

--- freent.c
+++ freent.c
@@ -1,4 +1,7 @@
 // FreeNT Kernel
 // 
 // This software comes with ABSOLUTELY NO WARRANTY. See the OpenWindows License
-// for details. https://github.com/freent-project/license-translations
+// for details.
+// 
+//   - Original OWL in English: https://github.com/freent-project/license-translations/raw/main/English/Original%20American%20English.license
+//   - Translations: https://github.com/freent-project/license-translations/blob/main/README.md#list-of-languages
